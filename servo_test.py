edc82e01dd59191d9281c5b2703697ea89f5c002
saraheh214 <117768399+saraheh214@users.noreply.github.com>
2024-11-24 12:26:53 -0500

    Create servo_test.py

---
+++ servo_test.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as  GPIO
+import time
+
+servoPin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPin, 50)
+p.start(2.5)
+
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.gleanup()
